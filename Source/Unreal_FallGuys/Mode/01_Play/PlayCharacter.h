e10721de9f3a6192bff74665c39d56e9be5aafbd
kcs7066 <kcs7066@naver.com>
2025-03-06 16:37:46 +0900

    김창수 : 다이브 수정

--- Source/Unreal_FallGuys/Mode/01_Play/PlayCharacter.h
+++ Source/Unreal_FallGuys/Mode/01_Play/PlayCharacter.h
@@ -74,6 +74,18 @@ public:
 		DiveTime = 0.0f;
 	}
 
+	UFUNCTION(BlueprintCallable)
+	void AddGetUpTime(float _Deltatime)
+	{
+		GetUpTime += _Deltatime;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void ResetGetUpTime(float _Deltatime)
+	{
+		GetUpTime = 0.0f;
+	}
+
 
 	FVector GetControllerForward();
 	FVector GetControllerRight();
@@ -113,5 +125,8 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float DiveTime = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float GetUpTime = 0.0f;
 
 };
