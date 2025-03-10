7e844ad2bb4b769f2a226c94666a413d9939c706
siasiaaksen <jj818181@naver.com>
2025-03-11 00:50:00 +0900

    이재영 : 플레이레벨 UI 클래스 BP 추가, 플레이레벨 대기 화면 위젯 이미지 배치 1차

---
+++ Source/Unreal_FallGuys/Mode/01_Play/UI/PlayUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_FALLGUYS_API UPlayUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
