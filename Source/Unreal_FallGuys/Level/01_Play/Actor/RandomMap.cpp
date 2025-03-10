4dbedb1ec542c16fe58f3cde90b804b15aa346ad
Minha Lee <dubidubs@naver.com>
2025-03-10 17:59:48 +0900

    이민하 : 슬라이더 오류 해결, 텍스트 렌더러로 큐브 구분, 탈락자 밀어내는 액터 추가, 망치 펀치로 충돌하도록 수정, 랜덤 맵 알고리즘 코드

---
+++ Source/Unreal_FallGuys/Level/01_Play/Actor/RandomMap.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Level/01_Play/Actor/RandomMap.h"
+#include <Kismet/GameplayStatics.h>
+
+// Sets default values
+ARandomMap::ARandomMap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARandomMap::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+
+	MapList.Add(TEXT("Zero"));
+	MapList.Add(TEXT("One"));
+	MapList.Add(TEXT("Two"));
+	
+	
+	
+}
+
+// Called every frame
+void ARandomMap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FString ARandomMap::GetRandomLevel()
+{
+	//if (UGameplayStatics::GetPlayerController(GetWorld(), 0) == GetOw GetController())
+	{
+
+
+
+		int Random = 0;
+		int RandomIndex = 0;
+		FString MapName = "";
+		do {
+			Random = FMath::RandRange(1, MapList.Num());
+			RandomIndex = Random - 1;
+
+		} while (PlayedMapList.Contains(RandomIndex)); // 이미 선택된 맵인지 확인
+
+		PlayedMapList.Add(RandomIndex); // 선택된 맵 추가
+		MapName = MapList[RandomIndex];
+
+		return MapList[RandomIndex]; // 랜덤으로 선택된 맵 반환
+
+
+
+	}
+}
+
