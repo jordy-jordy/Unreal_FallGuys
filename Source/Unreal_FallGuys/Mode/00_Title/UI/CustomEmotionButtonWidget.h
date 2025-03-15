33f8680e72eda60d875ba279e141e9cb3afd4d53
Minha Lee <fgz1234@gmail.com>
2025-03-15 14:12:17 +0900

    이민하 : 이모션, 패턴 버튼 추가, 위젯 구조 변경 후 오류나는 곳 해결

---
+++ Source/Unreal_FallGuys/Mode/00_Title/UI/CustomEmotionButtonWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Mode/00_Title/UI/CustomButtonWidget.h"
+#include "CustomEmotionButtonWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_FALLGUYS_API UCustomEmotionButtonWidget : public UCustomButtonWidget
+{
+	GENERATED_BODY()
+	
+};
