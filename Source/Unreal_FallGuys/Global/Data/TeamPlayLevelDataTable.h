916b3bef085f566bb42683809507d83af71a30b6
Jordy <303.jordy@gmail.com>
2025-04-02 14:16:07 +0900

    이현정 : 팀전용 레벨 데이터 테이블 분리

---
+++ Source/Unreal_FallGuys/Global/Data/TeamPlayLevelDataTable.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TeamPlayLevelDataTable.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FTeamPlayLevelDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FTeamPlayLevelDataRow() {}
+	~FTeamPlayLevelDataRow() {}
+
+public:
+	// 레벨
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	TSoftObjectPtr<UWorld> Level;
+
+	// 레벨 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	FString Name;
+
+	// 제한 시간 유무
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	bool UseLimitTime = false;
+
+	// Stage 플레이 제한 시간 - 패키징때 쓰는 값
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	float StageLimitTime = 60.0f;
+
+	// 전환 화면용 레벨 이미지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	UTexture2D* LevelIMG = nullptr;
+
+	// 플레이 가이드
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	FString PlayGuide;
+
+	// 플레이 화면에서 보여주는 목표
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TEAM LEVEL DATA")
+	FString GoalGuide;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_FALLGUYS_API UTeamPlayLevelDataTable : public UObject
+{
+	GENERATED_BODY()
+	
+};
