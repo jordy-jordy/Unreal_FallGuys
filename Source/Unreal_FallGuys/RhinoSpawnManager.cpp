f4859a8428e0eef427676dc633e735ea2e59c2d4
Minha Lee <dubidubs@naver.com>
2025-04-07 14:51:57 +0900

    이민하 : 임시커밋

---
+++ Source/Unreal_FallGuys/RhinoSpawnManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RhinoSpawnManager.h"
+
+// Sets default values
+ARhinoSpawnManager::ARhinoSpawnManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARhinoSpawnManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARhinoSpawnManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Unreal_FallGuys/RhinoSpawnManager.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RhinoSpawnManager.generated.h"
+
+UCLASS()
+class UNREAL_FALLGUYS_API ARhinoSpawnManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARhinoSpawnManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Rhino|Factory")
+	TSubclassOf<class AActor> RhinoFactory;
+
+};
