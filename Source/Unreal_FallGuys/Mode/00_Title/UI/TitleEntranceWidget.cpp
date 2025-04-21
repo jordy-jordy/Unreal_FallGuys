468a664d05407a1187a2b7d313fd0913a722b86e
siasiaaksen <jj818181@naver.com>
2025-04-21 18:48:56 +0900

    이재영 : 대기 위젯 버튼 이미지 변경 1차

--- Source/Unreal_FallGuys/Mode/00_Title/UI/TitleEntranceWidget.cpp
+++ Source/Unreal_FallGuys/Mode/00_Title/UI/TitleEntranceWidget.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Mode/00_Title/UI/TitleEntranceWidget.h"
+#include "Components/Button.h"
+#include "Styling/SlateBrush.h"
+#include "Mode/00_Title/UI/TitleMainWidget.h"
 
 
 void UTitleEntranceWidget::NativeConstruct()
@@ -9,4 +12,86 @@ void UTitleEntranceWidget::NativeConstruct()
 	Super::NativeConstruct();
 
 	bIsFocusable = false;
+
+	if (false == ArrButtons.IsEmpty())
+	{
+		CurButton = ArrButtons[0];
+	}
+}
+
+void UTitleEntranceWidget::WIdgetSelectInputAction(const FVector2D& _Value)
+{
+	if (GetMainWidget()->GetCurUIType() == ETitleUIType::TitleEntrance)
+	{
+		if (_Value.X < 0 && _Value.Y == 0.0f)
+		{
+			GetMainWidget()->SwitchWidget(ETitleUIType::TitleHome);
+		}
+	}
+}
+
+void UTitleEntranceWidget::WIdgetMoveInputAction(const FVector2D& _Value)
+{
+	if (GetMainWidget()->GetCurUIType() == ETitleUIType::TitleEntrance)
+	{
+		if (false == ArrButtons.IsEmpty() && true == IsInput)
+		{
+			if (_Value.X < 0 && _Value.Y == 0.0f)
+			{
+				CurIndex -= 1;
+
+				if (CurIndex < 0)
+				{
+					CurIndex = 0;
+				}
+			}
+
+			if (_Value.X > 0 && _Value.Y == 0.0f)
+			{
+				CurIndex += 1;
+
+				if (CurIndex > ArrButtons.Num() - 1)
+				{
+					CurIndex = ArrButtons.Num() - 1;
+				}
+			}
+
+			CurButton = ArrButtons[CurIndex];
+			PlayButtonAnim(CurIndex);
+		}
+	}
+}
+
+void UTitleEntranceWidget::PlayButtonAnim(int _CurIndex)
+{
+	switch (_CurIndex)
+	{
+	case 0:
+	{
+		PlayAnimation(StartServerButton);
+		break;
+	}
+	case 1:
+	{
+		PlayAnimation(CompeteButton);
+		break;
+	}
+	case 2:
+	{
+		PlayAnimation(ConnectButton);
+		break;
+	}
+	default:
+		break;
+	}
+
+	IsInput = false;
+}
+
+void UTitleEntranceWidget::ChangeButtonImage(UButton* _CurButton, class UObject* InResourceObject)
+{
+	FButtonStyle ButtonStyle = _CurButton->WidgetStyle;
+	FSlateBrush NormalBrush = _CurButton->WidgetStyle.Normal;
+	NormalBrush.SetResourceObject(InResourceObject);
+	ButtonStyle.SetNormal(NormalBrush);
 }
--- Source/Unreal_FallGuys/Mode/00_Title/UI/TitleEntranceWidget.h
+++ Source/Unreal_FallGuys/Mode/00_Title/UI/TitleEntranceWidget.h
@@ -29,6 +29,58 @@ public:
 		return TagetStage;
 	}
 
+	UFUNCTION(BlueprintCallable)
+	void WIdgetSelectInputAction(const FVector2D& _Value);
+
+	UFUNCTION(BlueprintCallable)
+	void WIdgetMoveInputAction(const FVector2D& _Value);
+
+	UFUNCTION(BlueprintCallable)
+	void PlayButtonAnim(int _CurIndex);
+
+	UFUNCTION(BlueprintCallable)
+	void SetInputState(bool _Value)
+	{
+		IsInput = _Value;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	int GetCurIndex()
+	{
+		return CurIndex;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void ChangeButtonImage(class UButton* _CurButton, class UObject* InResourceObject);
+
+	UFUNCTION(BlueprintCallable)
+	void SetButtonImage(class UImage* _ButtonImage)
+	{
+		ButtonImage = _ButtonImage;
+	}
+
 private:
 	EStageType TagetStage;
+
+	UPROPERTY(VisibleAnywhere, Transient, meta = (BindWidgetAnim), Category = "UI")
+	UWidgetAnimation* StartServerButton;
+
+	UPROPERTY(VisibleAnywhere, Transient, meta = (BindWidgetAnim), Category = "UI")
+	UWidgetAnimation* CompeteButton;
+
+	UPROPERTY(VisibleAnywhere, Transient, meta = (BindWidgetAnim), Category = "UI")
+	UWidgetAnimation* ConnectButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	TArray<class UButton*> ArrButtons;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	class UButton* CurButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	class UImage* ButtonImage;
+
+	int CurIndex = 0;
+
+	bool IsInput = false;
 };
